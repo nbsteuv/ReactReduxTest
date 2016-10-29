ce4730d41ed7717f1d348a09c5e9807f2f0e5650
nbsteuv <nbsteuv@gmail.com>
2016-10-29 11:48:00 -0400

    Used same method for search text change

--- app/app.jsx
+++ app/app.jsx
@@ -19,4 +19,4 @@ ReactDOM.render(
 	document.getElementById('app')
 );
 
-require('./redux-example.jsx');
+require('./redux-todo-example.jsx');
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -7,6 +7,13 @@ var storeDefault = {
 };
 
 var reducer = (state = storeDefault, action) => {
+  switch(action.type){
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      }
+  }
   return state;
 };
 
@@ -15,3 +22,12 @@ var store = redux.createStore(reducer);
 var currentState = store.getState();
 
 console.log(currentState);
+
+var changeSearchText = {
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'New text'
+};
+
+store.dispatch(changeSearchText);
+
+console.log(store.getState());
