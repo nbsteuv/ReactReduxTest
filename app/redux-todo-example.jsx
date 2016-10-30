6d5f547e56389e8f840cce2c9590be19b63e593d
nbsteuv <nbsteuv@gmail.com>
2016-10-30 16:35:17 -0400

    Added redux dev tools and subscribing to todos through function passing

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -17,24 +17,31 @@ var reducer = (state = storeDefault, action) => {
   return state;
 };
 
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
-var currentState = store.getState();
-
-console.log(currentState);
+store.subscribe(() => {
+  var state = store.getState();
+  document.getElementById('app').innerHTML = state.searchText;
+  console.log(state);
+});
 
 // var changeSearchText = {
 //   type: 'CHANGE_SEARCH_TEXT',
 //   searchText: 'New text'
 // };
 
-function changeSearchText(text){
+function createSearchTextDispatch(text){
   return {
       type: 'CHANGE_SEARCH_TEXT',
       searchText: text
   }
 }
 
-store.dispatch(changeSearchText('Text changed in function'));
+function changeSearchText(text){
+  store.dispatch(createSearchTextDispatch(text));
+}
 
-console.log(store.getState());
+changeSearchText('awegwoeig');
+changeSearchText('New Search Text');
